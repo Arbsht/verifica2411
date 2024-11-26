110a7877bdea68e8111089b8a4a3afe1559e9926
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2024-11-26 11:58:57 +0000

    5

--- b-end/index.js
+++ b-end/index.js
@@ -19,9 +19,18 @@ app.get('/css/stile.css', function (req, res) {
     app.use('/', express.static(__dirname + "/"));
     res.sendFile(path.join(__dirname,'www/css/stile.css'));
     });
-app.get('/api/mante', function (req, res) {
-    app.use('/', express.static(__dirname + "/"));
-    res.sendFile(path.join(__dirname,'www/mante.json'));
+app.get('/api/mante', (req, res) => {
+    fs.readFile('./www/mante.json', 'utf8', (err, data) => {
+    if (err) {
+    console.error('Errore durante la lettura del file JSON:', err);
+    return res.status(500).send('Errore interno del server');
+    }
+        
+    const productsData = JSON.parse(data);
+    console.log(typeof productsData);
+    res.setHeader('Content-Type', 'application/json');
+    res.json(productsData);
+    });
     });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
---
+++ f-end/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, provideHttpClient} from '@angular/common/http'; //HTTP CLIENT
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone:true,
+  imports:   
+ [CommonModule, HttpClientModule]
+})
+export class AppComponent implements OnInit{
+  
+  title = 'f-end';
+  mante: any[] = [];
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+  
+    this.http.get('https://3000-arbsht-verifica2411-g3oxdtkdci7.ws-eu116.gitpod.io/api/mante').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.mante = data['mante'];
+      console.log(data);
+    });
+  }
+
+}
