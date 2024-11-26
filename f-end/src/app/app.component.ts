e366d17f1fafd1d2f097f963e9476136823e4df0
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2024-11-26 12:13:37 +0000

    punto 7

--- f-end/src/app/app.component.ts
+++ f-end/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient, provideHttpClient} from '@angular/common/http'; //HTTP CLIENT
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { Mante } from './mante.model';
 
 @Component({
   selector: 'app-root',
@@ -14,17 +15,23 @@ export class AppComponent implements OnInit{
   
   title = 'f-end';
   yeah = false;
-  mante: any[] = [];
+  mante: Mante[] = [];
     // Inject HttpClient into your component or service.
     constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
   
-    this.http.get('https://3000-arbsht-verifica2411-g3oxdtkdci7.ws-eu116.gitpod.io/api/mante').subscribe(data => {
-      // Read the result field from the JSON response.
-      this.mante = data['mante'];
-      console.log(data);
-    });
+    this.http.get<{mante: Mante[]}>('https://3000-arbsht-verifica2411-g3oxdtkdci7.ws-eu116.gitpod.io/api/mante')
+      .subscribe({
+
+        next: (response) => {
+          this.mante = response.mante;
+          console.log('Received data:', this.mante);
+        },
+        error: (error) => {
+          console.error('Error fetching data:', error);
+        }
+      });
   }
 
   vediDati() {
---
+++ f-end/src/app/mante.model.ts
@@ -0,0 +1,10 @@
+export interface Mante {
+    _id: string;
+    estinsione: boolean;
+    apertura_alare: number;
+    immagine: string;
+    eta: number;
+    colore: string;
+    nome_specie: string;
+    nome_razza: string;
+}
