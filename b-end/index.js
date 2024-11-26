613afd370c0b417c1aa23bc2991df4a3ffa3ea4a
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2024-11-26 11:17:13 +0000

    punto 2

--- b-end/index.js
+++ b-end/index.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var app = express(); 
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const app= express();
+const fs = require('fs');
+
+var cors = require('cors'); //HTTP access control (CORS) for cross origin requests
+
+app.use(cors()); //Setup cors
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+    app.use('/', express.static(__dirname + "/"));
+    res.sendFile(path.join(__dirname,'www/index.html'));
+    });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
